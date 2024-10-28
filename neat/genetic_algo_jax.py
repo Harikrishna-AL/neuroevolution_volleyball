f45e67065b00f01c669a77de840dd87f5961fc45
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2024-10-28 18:09:10 +0530

    adjust distance threshold

--- neat/genetic_algo_jax.py
+++ neat/genetic_algo_jax.py
@@ -337,7 +337,7 @@ class GeneticEvolution:
                     ]
                 ),
             )
-            # print("Distances: ",distances)
+            print("Distances: ",distances)
             # print("Distances shape: ",distances.shape)
             found = jnp.any(distances < 0.8)
             if found:
